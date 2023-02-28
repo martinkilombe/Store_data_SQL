daf291a64b6fb35a318c73c2a683d70f9c7a5f59
martin kilombe <martinkilombe@gmail.com>
2023-03-01 00:29:51 +0300

    Added expenses column and updated

--- stores.sql
+++ stores.sql
@@ -16,10 +16,16 @@ SET data_store.orders.Order_Date = STR_TO_DATE(data_store.orders.Order_Date, '%d
 #Calculate the Gross revenue AND create a new column for this
 ALTER TABLE data_store.orders
 ADD COLUMN Gross_revenue INTEGER;
-
 #Updating the Gross_revenue table
 UPDATE data_store.orders
 SET data_store.orders.Gross_revenue = sales*Quantity;
+
+#calulate the Expenses calculates as ; Gross_revenue- Expenses and add to a new column
+ALTER TABLE data_store.orders
+ADD COLUMN Expenses integer;
+#update the column Expenses with values
+UPDATE data_store.orders
+SET data_store.orders.Expenses = data_store.orders.Gross_revenue - data_store.orders.Profit;
 
 
 
