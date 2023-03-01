78d7284558886c0d3db8b95a7536e2d7b6a74e87
martin kilombe <martinkilombe@gmail.com>
2023-03-01 18:25:10 +0300

    Removed date_difference column

--- stores.sql
+++ stores.sql
@@ -1,18 +1,6 @@
 SELECT * FROM data_store.orders;
 SELECT * FROM data_store.returns;
 
-#Add a column that determines the difference in days between the order_date and ship_date
-#Create a new column that handles the difference
-ALTER TABLE data_store.orders
-ADD COLUMN Date_Difference INTEGER;
-#Calculate date difference and add to the new column-- we get an error message due to the date format
-UPDATE data_store.orders 
-SET Date_difference = datediff(data_store.orders.Ship_Date,data_store.orders.Order_Date);
-
-#Converting it to the m/d/yyyymfor the two columns
-UPDATE data_store.orders 
-SET data_store.orders.Order_Date = STR_TO_DATE(data_store.orders.Order_Date, '%d/%m/%Y');
-
 #Calculate the Gross revenue AND create a new column for this
 ALTER TABLE data_store.orders
 ADD COLUMN Gross_revenue INTEGER;
@@ -26,6 +14,8 @@ ADD COLUMN Expenses integer;
 #update the column Expenses with values
 UPDATE data_store.orders
 SET data_store.orders.Expenses = data_store.orders.Gross_revenue - data_store.orders.Profit;
+
+
 
 
 
